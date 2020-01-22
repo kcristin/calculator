065e2f1d5c0dabcff007dcaf2842f422397064cc
Kyle Cristin <kyle.rf.cristin@gmail.com>
2020-01-21 16:38:18 -0800

    First commit

---
+++ functions.js
@@ -0,0 +1,39 @@
+let operate = (operator, int1, int2) => {
+    switch (operator) {
+        case '*':
+            console.log("MULTIPLY");
+            multiply(int1, int2);
+            break;
+
+        case '/':
+            console.log("DIVIDE");
+            divide(int1, int2);
+            break;
+
+        case '+':
+            console.log("ADD");
+            add(int1, int2);
+            break;
+
+        case '-':
+            console.log("SUBTRACT");
+            subtract(int1, int2);
+            break;
+    }
+}
+
+let add = (int1, int2) => {
+    return int1 + int2;
+}
+
+let subtract = (int1, int2) => {
+    return int1 - int2;
+}
+
+let multiply = (int1, int2) => {
+    return int1 * int2;
+}
+
+let divide = (int1, int2) => {
+    return int1 / 2;
+}
