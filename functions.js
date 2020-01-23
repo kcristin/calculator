8ac0b5ab49faf82c7ee7aa53bbde0359e62c760c
Kyle Cristin <kyle.rf.cristin@gmail.com>
2020-01-23 13:05:32 -0800

    Implement order of operations

--- functions.js
+++ functions.js
@@ -1,39 +1,105 @@
+
+// Implements order of operation
+let pemdas = (inputArray) => {
+    for (let j = 0; j < inputArray.length; j++) {
+        if (inputArray[j] === '*') {
+            let operator = inputArray[j];
+            let int1 = inputArray[j - 1];
+            let int2 = inputArray[j + 1];
+            let newVal = operate(operator, int1, int2);
+
+            inputArray.splice((j-1), 3, newVal);
+            console.log(inputArray);
+        
+        } else {
+            continue;
+        }
+    }
+
+    for (let j = 0; j < inputArray.length; j++) {
+        if (inputArray[j] === '/') {
+            let operator = inputArray[j];
+            let int1 = inputArray[j - 1];
+            let int2 = inputArray[j + 1];
+            let newVal = operate(operator, int1, int2);
+
+            inputArray.splice((j-1), 3, newVal);
+            console.log(inputArray);
+        
+        } else {
+            continue;
+        }
+    }
+
+    for (let j = 0; j < inputArray.length; j++) {
+        if (inputArray[j] === '+') {
+            let operator = inputArray[j];
+            let int1 = inputArray[j - 1];
+            let int2 = inputArray[j + 1];
+            let newVal = operate(operator, int1, int2);
+
+            inputArray.splice((j-1), 3, newVal);
+            console.log(inputArray);
+        
+        } else {
+            continue;
+        }
+    }
+
+    for (let j = 0; j < inputArray.length; j++) {
+        if (inputArray[j] === '-') {
+            let operator = inputArray[j];
+            let int1 = inputArray[j - 1];
+            let int2 = inputArray[j + 1];
+            let newVal = operate(operator, int1, int2);
+
+            inputArray.splice((j-1), 3, newVal);
+            console.log(inputArray);
+        
+        } else {
+            continue;
+        }
+    }
+    return inputArray[0];
+}
+
+// Calls appropriate function given operator
 let operate = (operator, int1, int2) => {
     switch (operator) {
         case '*':
             console.log("MULTIPLY");
-            multiply(int1, int2);
+            return multiply(int1, int2);
             break;
 
         case '/':
             console.log("DIVIDE");
-            divide(int1, int2);
+            return divide(int1, int2);
             break;
 
         case '+':
             console.log("ADD");
-            add(int1, int2);
+            return add(int1, int2);
             break;
 
         case '-':
             console.log("SUBTRACT");
-            subtract(int1, int2);
+            return subtract(int1, int2);
             break;
     }
 }
 
 let add = (int1, int2) => {
-    return console.log(+int1 + +int2);
+    return +int1 + +int2;
 }
 
 let subtract = (int1, int2) => {
-    return console.log(int1 - int2);
+    return int1 - int2;
 }
 
 let multiply = (int1, int2) => {
-    return console.log(int1 * int2);
+    return int1 * int2;
 }
 
 let divide = (int1, int2) => {
-    return console.log(int1 / 2);
+    return int1 / int2;
 }
